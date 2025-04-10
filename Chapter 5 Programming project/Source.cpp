0b898f3465ae090865ba1b8afe17b3271527ad81
AlexanderThompson47 <thompsonalexander287@gmail.com>
2025-04-10 02:32:12 -0400

    finished program 4

--- Chapter 5 Programming project/Source.cpp
+++ Chapter 5 Programming project/Source.cpp
@@ -1,37 +1,29 @@
-//this is program 3 population bar chart
+//this is program 4 patterns
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
-
 using namespace std;
-//you can enter whatever you want for the town name but you have to enter "People.txt" or the numbers wont show up
-//this part is where you enter the data
+
 int main() {
-    string townName, fileName;
-    cout << "Enter the name of the town (whatever you want): ";
-    getline(cin, townName);
-    cout << "Enter the name of the data file (People.txt or it will give error): ";
-    cin >> fileName;
-    //the error happens when you put the fileName in wrong
-    ifstream dataFile(fileName);
+    const int numRows = 10;
 
-    if (!dataFile) {
-        cerr << "Error opening file: " << fileName << endl;
-        return 1;
-    }
-
-    cout << "\nPopulation Growth Bar Chart for " << townName << "\n" << endl;
-
-    int year, population;
-    while (dataFile >> year >> population) {
-        cout << year << " | ";
-        for (int i = 0; i < population / 1000; i++) {
-            cout << '*';
+    // Pattern A
+    cout << "Pattern A\n";
+    for (int i = 1; i <= numRows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "+";
         }
-        cout << " (" << population << ")" << endl;
+        cout << endl;
     }
 
-    dataFile.close();
+    cout << endl; // Separate patterns with a blank line
+
+    // Pattern B
+    cout << "Pattern B\n";
+    for (int i = numRows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
