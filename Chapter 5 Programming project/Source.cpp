8bf9a0040976a4be1ee39a6d4602508f9cd38474
AlexanderThompson47 <thompsonalexander287@gmail.com>
2025-04-10 03:21:07 -0400

    finish program 5

--- Chapter 5 Programming project/Source.cpp
+++ Chapter 5 Programming project/Source.cpp
@@ -1,29 +1,36 @@
-//this is program 4 patterns
+//this is program 5 random number guessing game
 #include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
 using namespace std;
 
 int main() {
-    const int numRows = 10;
+    // Seed the random number generator with the current time
+    srand(static_cast<unsigned int>(time(0)));
+    int randomNumber = rand() % 100 + 1; // Generate a random number between 1 and 100
 
-    // Pattern A
-    cout << "Pattern A\n";
-    for (int i = 1; i <= numRows; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << "+";
+    int userGuess;
+    int guessCount = 0;
+
+    cout << "I have generated a random number between 1 and 100.\n";
+    cout << "Try to guess it!" << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> userGuess;
+        guessCount++;
+
+        if (userGuess > randomNumber) {
+            cout << "Too high, try again." << endl;
         }
-        cout << endl;
-    }
-
-    cout << endl; // Separate patterns with a blank line
-
-    // Pattern B
-    cout << "Pattern B\n";
-    for (int i = numRows; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
-            cout << "+";
+        else if (userGuess < randomNumber) {
+            cout << "Too low, try again." << endl;
         }
-        cout << endl;
-    }
+    } while (userGuess != randomNumber);
+
+    cout << "Congratulations! You guessed the number!" << endl;
+    cout << "It took you " << guessCount << " guesses." << endl;
 
     return 0;
 }
